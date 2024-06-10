ea41bb9476138290b28227b9390aa1b3890b8442
Mar <mar@strawmelonjuice.com>
2024-06-10 11:48:34 +0200

    Give it it's own server

---
+++ bunserve.js
@@ -0,0 +1,21 @@
+const BASE_PATH = "./public";
+console.log(
+  "Server is running on http://localhost:3000, serving files from " + BASE_PATH,
+);
+Bun.serve({
+  port: 3000,
+  async fetch(req) {
+    let filePath;
+    if (new URL(req.url).pathname === "/") {
+      filePath = BASE_PATH + "/index.html";
+    } else {
+      filePath = BASE_PATH + new URL(req.url).pathname;
+    }
+    console.log("Requesting file", filePath);
+    const file = Bun.file(filePath);
+    return new Response(file);
+  },
+  error() {
+    return new Response(null, { status: 404 });
+  },
+});
---
+++ public/scripts/script.js
@@ -0,0 +1,66 @@
+/* 
+    Author: MLC Bloeiman <@strawmelonjuice, mar@strawmelonjuice.com> 
+    Date: <date>
+    
+    <Subject>
+
+*/
+var request = new XMLHttpRequest();
+// Ophalen van data.json voor de "dynamische variabelen", zo hoef ik niet de hele js te openen als ik een pagina toevoeg.
+request.open("GET", "/data.json", true);
+request.onload = function () {
+  if (request.status >= 200 && request.status < 400) {
+    ((globaleVariabelen) => {
+      console.log(request.responseText);
+      const pageTitle = (() => {
+        console.log(globaleVariabelen);
+        const element = document.createElement("p");
+        element.innerHTML = "&ensp;&mdash;&ensp;Mar&#39;s&nbsp;site";
+        return document
+          .querySelector("title")
+          .innerText.replace(element.innerText, "");
+      })();
+      document.querySelector("header h1").innerText = pageTitle;
+
+      const menu = document.getElementById("menu-items");
+
+      document.getElementById("closemenu").addEventListener("click", () => {
+        document.getElementById("menu").classList.remove("ready");
+        setTimeout(() => {
+          document.getElementById("menu").classList.remove("unfolded");
+          document.getElementById("menu").classList.add("ready");
+        }, 100);
+      });
+
+      document
+        .querySelector("#menu.ready:not(:hover, .unfolded)")
+        .addEventListener("click", () => {
+          document.getElementById("menu").classList.add("unfolded");
+        });
+
+      for (item of globaleVariabelen.menuItems) {
+        const menuItem = document.createElement("li");
+        menuItem.innerHTML = `<a href="${item.link}">${item.title}</a>`;
+        if (
+          (item.title === "Home" &&
+            (window.location.pathname === "/" ||
+              window.location.pathname === "/index.html")) ||
+          window.location.pathname === item.link
+        ) {
+          menuItem.classList.add("active");
+        }
+        menu.appendChild(menuItem);
+      }
+
+      if (pageTitle === "Results") {
+        console.log("This is the results page...");
+        const mainElem = document.querySelector("main>div");
+        mainElem.innerHTML = "<p>Loading results...</p>";
+      }
+    })(JSON.parse(request.responseText));
+  } else {
+    window.location.assign("/pages/error.html");
+  }
+};
+request.onerror = function () {};
+request.send();
